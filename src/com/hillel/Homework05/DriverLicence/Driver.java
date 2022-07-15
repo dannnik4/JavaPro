f3620f066c1c1cf1ea4c27dbc7ac7eb50cc22c86
dannnik4 <daniilperstnev2001@gmail.com>
2022-07-15 22:47:40 +0300

    Add Homework 05

--- src/com/hillel/Homework05/DriverLicence/Driver.java
+++ src/com/hillel/Homework05/DriverLicence/Driver.java
@@ -1,45 +1,11 @@
 package com.hillel.Homework05.DriverLicence;
 
-import com.hillel.Homework05.CatList.Cat;
-
 public class Driver {
 
     public String name;
     public boolean driversLicense;
     public int age;
     public int noFinesDays;
-
-    public static Driver createRandomDriver() {
-
-        int age = (int) (Math.random() * 15) + 1;
-        int rColor = (int) (Math.random() * 6);
-        int rCity = (int) (Math.random() * 10);
-        String[] colors = {
-                "White",
-                "Black",
-                "Grey",
-                "Blue",
-                "Green",
-                "Yellow"
-        };
-
-        String[] cities = {
-                "Kyiv",
-                "Sevastopol",
-                "Kharkiv",
-                "Odessa",
-                "Zaporizhzhya",
-                "Lviv",
-                "Donetsk",
-                "Kryvyy Rih",
-                "Dnipro",
-                "Mykolayiv"};
-
-        String color = colors[rColor];
-        String address = cities[rCity];
-
-        return new Cat(color, age, address);
-    }
 
     public Driver(String name, boolean driverLicense, int age, int noFinesDays) {
         this.name = name;
